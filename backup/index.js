533639d99c8605c817ea51c62278c6e8d24ffaf6
TheConnMan <bcconn2112@gmail.com>
2016-10-16 09:37:58 -0400

    Zip backup archive as a file

--- backup/index.js
+++ backup/index.js
@@ -1,10 +1,18 @@
 var config = require('./config');
 var firebase = require('./lib/firebase');
+var fs = require('fs');
 
 exports.handler = function(event, context, callback) {
 
     Promise.all([firebase.getFoosers(), firebase.getTeams(), firebase.getGames()]).then(function(data) {
-        console.log(data);
+        var zip = new require('node-zip')();
+        zip.file('foosballers.json', JSON.stringify(data[0]));
+        zip.file('games.json', JSON.stringify(data[1]));
+        zip.file('teams.json', JSON.stringify(data[2]));
+        var file = fs.writeFileSync('data.zip', zip.generate({
+            base64: false,
+            compression: 'DEFLATE'
+        }), 'binary');
         callback(null, "Backup complete");
     });
 };
